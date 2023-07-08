52c6ecbd63d2cb32d2c1b9e6212c9e71d5894964
Anton Trafimovich <tonytrof@gmail.com>
2023-07-08 14:22:10 +0200

    feat: provide env vars to lambda to access postgress.

--- cdk.ts
+++ cdk.ts
@@ -12,4 +12,11 @@ new NodejsFunction(stack, 'cartServiceLambda', {
   runtime: lambda.Runtime.NODEJS_18_X,
   entry: 'dist/main.js',
   handler: 'handler',
+  environment: {
+    PGUSER: process.env.PGUSER as string,
+    PGHOST: process.env.PGHOST as string,
+    PGPASSWORD: process.env.PGPASSWORD as string,
+    PGDATABASE: process.env.PGDATABASE as string,
+    PGPORT: process.env.PGPORT as string,
+  },
 });
