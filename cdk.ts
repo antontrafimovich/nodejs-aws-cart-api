56d972a65280aa51d28a6a419dcbad1f5942f9dc
Anton Trafimovich <tonytrof@gmail.com>
2023-07-08 14:16:37 +0200

    feat: add ability to deploy built nestjs app to lambda.

--- cdk.ts
+++ cdk.ts
@@ -8,14 +8,8 @@ const stack = new cdk.Stack(app, 'CartServiceStack', {
   env: { region: process.env.AWS_REGION as string },
 });
 
-const cartServiceLambdaLayer = new lambda.LayerVersion(stack, 'cartServiceLayer', {
-  code: lambda.Code.fromAsset('node_modules'),
-  compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
-});
-
 new NodejsFunction(stack, 'cartServiceLambda', {
   runtime: lambda.Runtime.NODEJS_18_X,
-  entry: 'dist/src/handler.js',
+  entry: 'dist/main.js',
   handler: 'handler',
-  layers: [cartServiceLambdaLayer],
 });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { Express } from 'express';
+import { createServer, proxy, Response } from '@vendia/serverless-express';
+import express from 'express';
+import { Server } from 'http';
 
 import { AppModule } from './app.module';
 
 export async function createApp(
-  expressApp: Express,
+  expressApp: express.Express,
 ): Promise<INestApplication> {
   const app = await NestFactory.create(
     AppModule,
@@ -16,4 +18,24 @@ export async function createApp(
   app.init();
 
   return app;
+}
+
+let cachedServer: Server;
+
+async function bootstrap(): Promise<Server> {
+  const expressApp = express();
+
+  const app = await createApp(expressApp);
+  await app.init();
+
+  return createServer(expressApp);
+}
+
+export async function handler(event: any, context): Promise<Response> {
+  if (!cachedServer) {
+    const server = await bootstrap();
+    cachedServer = server;
+  }
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
 }
