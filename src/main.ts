1b43dd59626d940aa9939b1776194aa1d23f5a0d
Anton Trafimovich <tonytrof@gmail.com>
2023-07-09 20:46:46 +0200

    fix: change the way handler is being registered.

--- src/main.ts
+++ src/main.ts
@@ -5,33 +5,17 @@ import 'dotenv/config';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
-// let server: any;
+let server: any;
 
-// async function bootstrap(): Promise<any> {
-//   const app = await NestFactory.create(AppModule);
-//   await app.init();
-
-//   const expressApp = app.getHttpAdapter().getInstance();
-//   return serverlessExpress({ app: expressApp });
-// }
-
-// export const handler = async (event: any, context: any, callback: any) => {
-//   server = server ?? (await bootstrap());
-//   return server(event, context, callback);
-// };
-
-const port = process.env.PORT || 4000;
-
-async function bootstrap() {
+async function bootstrap(): Promise<any> {
   const app = await NestFactory.create(AppModule);
+  await app.init();
 
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
-  app.use(helmet());
-
-  await app.listen(port);
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
+
+export const handler = async (event: any, context: any) => {
+  server = server ?? (await bootstrap());
+  return server(event, context);
+};
