93e2ffde10448b2f68e349ab99a31709f0f6d2e6
Anton Trafimovich <tonytrof@gmail.com>
2023-07-09 20:10:56 +0200

    feat: add api gateway as proxy to pass all the args to lambda.

--- cdk.ts
+++ cdk.ts
@@ -1,3 +1,7 @@
+import 'dotenv/config';
+
+import * as apigateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
@@ -8,7 +12,7 @@ const stack = new cdk.Stack(app, 'CartServiceStack', {
   env: { region: process.env.AWS_REGION as string },
 });
 
-new NodejsFunction(stack, 'cartServiceLambda', {
+const cartServiceLambda = new NodejsFunction(stack, 'cartServiceLambda', {
   runtime: lambda.Runtime.NODEJS_18_X,
   entry: 'dist/main.js',
   handler: 'handler',
@@ -19,4 +23,22 @@ new NodejsFunction(stack, 'cartServiceLambda', {
     PGDATABASE: process.env.PGDATABASE as string,
     PGPORT: process.env.PGPORT as string,
   },
+});
+
+const http = new apigateway.HttpApi(stack, 'CartServiceHTTP', {
+  corsPreflight: {
+    allowOrigins: ['*'],
+    allowMethods: [apigateway.CorsHttpMethod.ANY],
+    allowHeaders: ['*'],
+  },
+});
+
+const importCSVIntegration = new HttpLambdaIntegration(
+  'cartServiceIntegration',
+  cartServiceLambda,
+);
+http.addRoutes({
+  path: '/{proxy+}',
+  integration: importCSVIntegration,
+  methods: [apigateway.HttpMethod.ANY],
 });
