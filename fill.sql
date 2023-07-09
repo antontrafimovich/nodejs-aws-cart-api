d4e09a083fe9063239060a031537033708d78b0d
Anton Trafimovich <tonytrof@gmail.com>
2023-07-09 20:57:16 +0200

    feat: add query to fill table.

---
+++ fill.sql
@@ -0,0 +1,4 @@
+insert into cart (created_at, updated_at, status) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', 'c528d133-1c6f-4368-8b8d-ef44d1593086', '1/18/2023', '1/18/2023', 'OPEN');
+insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '7e87f525-22f8-40ae-924c-48d67d38bae3', 140);
+insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '8c1f4f24-78d3-455b-b8b7-68dfa9ed70a0', 12);
+insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '9978cf54-4b14-4721-9b6d-18c9173429a4', 225);
