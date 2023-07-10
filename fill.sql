2e58df862fa62b9abb6e14f04da14c9bf195328e
Anton Trafimovich <tonytrof@gmail.com>
2023-07-10 19:19:32 +0200

    fix: update fill.sql script.

--- fill.sql
+++ fill.sql
@@ -1,4 +1,4 @@
-insert into cart (created_at, updated_at, status) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', 'c528d133-1c6f-4368-8b8d-ef44d1593086', '1/18/2023', '1/18/2023', 'OPEN');
-insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '7e87f525-22f8-40ae-924c-48d67d38bae3', 140);
-insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '8c1f4f24-78d3-455b-b8b7-68dfa9ed70a0', 12);
-insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '9978cf54-4b14-4721-9b6d-18c9173429a4', 225);
+insert into cart (id, user_id, created_at, updated_at, status) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '50028406-3103-48fc-a7fb-a0d15f0ae44f', '1/18/2023', '1/18/2023', 'OPEN');
+insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '45987655-cae1-4bba-8e8f-0cfc866aa453', 120);
+insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '3bab1006-bfa4-4ba4-a6bd-7ce49c73d4dc', 94);
+insert into cart_item (cart_id, product_id, count) values ('3412fec8-fa94-4974-96c3-bc2626f168f9', '334e1d81-3eb0-4f0b-893d-8d9eb44b7fc5', 12);
