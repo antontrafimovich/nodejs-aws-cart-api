a3099d3fda89060088aa59c9013fa15d719d9476
Anton Trafimovich <tonytrof@gmail.com>
2023-07-13 23:03:47 +0200

    feat: and and use rds user table.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { UsersService } from './services';
+import { DbModule } from '../db';
 
 @Module({
+  imports: [DbModule],
   providers: [ UsersService ],
   exports: [ UsersService ],
 })
