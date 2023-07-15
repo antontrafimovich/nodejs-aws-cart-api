4d2c4fa636369c34bf24fe0fa0c9603ba5849dd1
Anton Trafimovich <tonytrof@gmail.com>
2023-07-15 12:19:12 +0200

    feat: add aws handler to main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,19 @@ export const handler = async (event: any, context: any) => {
   server = server ?? (await bootstrap());
   return server(event, context);
 };
+
+// const port = process.env.PORT || 4000;
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: (req, callback) => callback(null, true),
+//   });
+//   app.use(helmet());
+
+//   await app.listen(port);
+// }
+// bootstrap().then(() => {
+//   console.log('App is running on %s port', port);
+// });
